cf7db5d4f8c805b930f35b6fba43e6d95c9984e6
Koussai21 <koussailaribi21@gmail.com>
2024-03-16 13:35:56 +0100

    Base

---
+++ app/src/main/java/com/example/testform/Voyage.kt
@@ -0,0 +1,9 @@
+package com.example.testform
+
+data class Voyage(
+    val pays: String,
+    val ville: String,
+    val monuments: String,
+    val avis: String,
+    val imageUri: String? // Ajoutez la propriété pour stocker l'URI de l'image
+)
